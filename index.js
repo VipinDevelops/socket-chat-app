3b4ab29df75c813da4cbeae7d1301940f3072c59
imvipindev <vipinchaudhary1809@gmail.com>
2023-04-15 12:41:31 +0530

    feat(setup): :construction: created html page with bootstrap enabled

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-import express from 'express';
+const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => 
+    res.sendFile(__dirname + '/client/index.html')
+);
+
 
 app.listen(port, () => console.log(`listening on port ${port}!`));
